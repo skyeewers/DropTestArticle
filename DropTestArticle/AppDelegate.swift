34af5608c1d226f733671f8036e643027866b5fb
Sven Ewers <sven.ewers@bearlydigital.dev>
2020-04-27 13:46:25 +0200

    Initial Commit

---
+++ DropTestArticle/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DropTestArticle
+//
+//  Created by Sven Ewers on 27.04.20.
+//  Copyright © 2020 Bearly Digital. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DropTestArticleTests/DropTestArticleTests.swift
@@ -0,0 +1,34 @@
+//
+//  DropTestArticleTests.swift
+//  DropTestArticleTests
+//
+//  Created by Sven Ewers on 27.04.20.
+//  Copyright © 2020 Bearly Digital. All rights reserved.
+//
+
+import XCTest
+@testable import DropTestArticle
+
+class DropTestArticleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
